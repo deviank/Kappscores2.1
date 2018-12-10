01c0baca13d403206a144031acf877067fcef9be
Devian <deviank@live.co.za>
2018-12-10 08:21:53 +0200

    adding google fonts and fallbacks.

--- wp-content/themes/kappscores/functions.php
+++ wp-content/themes/kappscores/functions.php
@@ -120,6 +120,9 @@ add_action( 'widgets_init', 'kappscores_widgets_init' );
  * Enqueue scripts and styles.
  */
 function kappscores_scripts() {
+    //Enqueue Google Fonts: Source Sans Pro and PT Serif
+    wp_enqueue_style('kappascores-font', 'https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i|Source+Sans+Pro:400,400i,600');
+
 	wp_enqueue_style( 'kappscores-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'kappscores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );

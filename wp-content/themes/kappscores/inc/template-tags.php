2d2692400337cd46ac8bb8e5f21e80c0a87ee551
Devian <deviank@live.co.za>
2019-01-24 18:48:52 +0200

    adding categories in header area and only tags in the footer.

--- wp-content/themes/kappscores/inc/template-tags.php
+++ wp-content/themes/kappscores/inc/template-tags.php
@@ -56,14 +56,9 @@ if ( ! function_exists( 'kappscores_entry_footer' ) ) :
 	 * Prints HTML with meta information for the categories, tags and comments.
 	 */
 	function kappscores_entry_footer() {
-		// Hide category and tag text for pages.
+		// Hide tag text for pages.
 		if ( 'post' === get_post_type() ) {
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'kappscores' ) );
-			if ( $categories_list ) {
-				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'kappscores' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-			}
+
 
 			/* translators: used between list items, there is a space after the comma */
 			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'kappscores' ) );
@@ -110,6 +105,18 @@ if ( ! function_exists( 'kappscores_entry_footer' ) ) :
 		);
 	}
 endif;
+
+/**
+ * Display category list
+ */
+
+function kappscores_the_category_list(){
+    $categories_list = get_the_category_list( esc_html__( ', ', 'kappscores' ) );
+    if ( $categories_list ) {
+        /* translators: 1: list of categories. */
+        printf( '<span class="cat-links">' . esc_html__( '%1$s', 'kappscores' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+    }
+}
 
 if ( ! function_exists( 'kappscores_post_thumbnail' ) ) :
 	/**

1302e149d42bc6079f6bd7b5f85deec0e8f8fa7a
Devian <deviank@live.co.za>
2019-02-14 07:08:06 +0200

    removing undefined function call -post-thumbnail-.

--- wp-content/themes/kappscores/template-parts/content-page.php
+++ wp-content/themes/kappscores/template-parts/content-page.php
@@ -1,52 +1,43 @@
 <?php
 /**
- * Template part for displaying page content in page.php
+ * Template part for displaying page content in page.php.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package Kappscores
+ * @package kappscores
  */
 
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+    <header class="entry-header">
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    </header><!-- .entry-header -->
 
-	<?php kappscores_post_thumbnail(); ?>
+    <div class="entry-content">
+        <?php
+        the_content();
 
-	<div class="entry-content">
-		<?php
-		the_content();
+        wp_link_pages( array(
+            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kappscores' ),
+            'after'  => '</div>',
+        ) );
+        ?>
+    </div><!-- .entry-content -->
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kappscores' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'kappscores' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+    <?php if ( get_edit_post_link() ) : ?>
+        <footer class="entry-footer">
+            <?php
+            edit_post_link(
+                sprintf(
+                /* translators: %s: Name of current post */
+                    esc_html__( 'Edit %s', 'kappscores' ),
+                    the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                ),
+                '<span class="edit-link">',
+                '</span>'
+            );
+            ?>
+        </footer><!-- .entry-footer -->
+    <?php endif; ?>
+</article><!-- #post-## -->

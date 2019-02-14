5bf52c15efa6707bfefde694f3f295302864dbb1
Devian <deviank@live.co.za>
2019-02-14 07:24:08 +0200

    increasing size for comments avatar.

--- wp-content/themes/kappscores/comments.php
+++ wp-content/themes/kappscores/comments.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * The template for displaying comments
+ * The template for displaying comments.
  *
  * This is the template that displays the area of the page that contains both the current comments
  * and the comment form.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package Kappscores
+ * @package kappscores
  */
 
 /*
@@ -16,60 +16,71 @@
  * return early without loading the comments.
  */
 if ( post_password_required() ) {
-	return;
+    return;
 }
 ?>
 
 <div id="comments" class="comments-area">
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
-		<h2 class="comments-title">
-			<?php
-			$kappscores_comment_count = get_comments_number();
-			if ( '1' === $kappscores_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'kappscores' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $kappscores_comment_count, 'comments title', 'kappscores' ) ),
-					number_format_i18n( $kappscores_comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
+    <?php
+    // You can start editing here -- including this comment!
+    if ( have_comments() ) : ?>
+        <h2 class="comments-title">
+            <?php
+            printf( // WPCS: XSS OK.
+                esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'kappscores' ) ),
+                number_format_i18n( get_comments_number() ),
+                '<span>' . get_the_title() . '</span>'
+            );
+            ?>
+        </h2>
 
-		<?php the_comments_navigation(); ?>
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+            <nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
+                <h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'kappscores' ); ?></h2>
+                <div class="nav-links">
 
-		<ol class="comment-list">
-			<?php
-			wp_list_comments( array(
-				'style'      => 'ol',
-				'short_ping' => true,
-			) );
-			?>
-		</ol><!-- .comment-list -->
+                    <div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'kappscores' ) ); ?></div>
+                    <div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'kappscores' ) ); ?></div>
 
-		<?php
-		the_comments_navigation();
+                </div><!-- .nav-links -->
+            </nav><!-- #comment-nav-above -->
+        <?php endif; // Check for comment navigation. ?>
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'kappscores' ); ?></p>
-			<?php
-		endif;
+        <ol class="comment-list">
+            <?php
+            wp_list_comments( array(
+                'style'      => 'ol',
+                'short_ping' => true,
+                'avatar_size' => '96'
+            ) );
+            ?>
+        </ol><!-- .comment-list -->
 
-	endif; // Check for have_comments().
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+            <nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+                <h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'kappscores' ); ?></h2>
+                <div class="nav-links">
 
-	comment_form();
-	?>
+                    <div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'kappscores' ) ); ?></div>
+                    <div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'kappscores' ) ); ?></div>
+
+                </div><!-- .nav-links -->
+            </nav><!-- #comment-nav-below -->
+        <?php
+        endif; // Check for comment navigation.
+
+    endif; // Check for have_comments().
+
+
+    // If comments are closed and there are comments, let's leave a little note, shall we?
+    if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+
+        <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'kappscores' ); ?></p>
+    <?php
+    endif;
+
+    comment_form();
+    ?>
 
 </div><!-- #comments -->

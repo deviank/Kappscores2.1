f72a988c836c2864c1038fb528508a39a2c2ece9
Devian <deviank@live.co.za>
2019-02-21 09:17:42 +0200

    align footer content.

--- wp-content/themes/kappscores/footer.php
+++ wp-content/themes/kappscores/footer.php
@@ -1,44 +1,32 @@
 <?php
 /**
- * The template for displaying the footer
+ * The template for displaying the footer.
  *
  * Contains the closing of the #content div and all content after.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Kappscores
+ * @package kappscores
  */
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-    <?php get_sidebar('footer');?>
+<?php get_sidebar( 'footer' ); ?>
 
-	<footer id="colophon" class="site-footer">
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-footer__wrap">
+        <nav class="social-menu">
+            <?php wp_nav_menu( array( 'theme_location' => 'social' ) ); ?>
+        </nav><!-- .social-menu -->
 
-     <nav class="social-menu">
-        <?php
-        wp_nav_menu( array(
-            'theme_location' => 'social',
-        ) );
-        ?>
-     </nav>
-
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'kappscores' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'kappscores' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'kappscores' ), 'kappscores', '<a href="https://www.devian.co.za">Devian Kapp</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+        <div class="site-info">
+            <div><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'kappscores' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'kappscores' ), 'WordPress' ); ?></a></div>
+            <div><?php printf( esc_html__( 'Theme: %1$s by %2$s', 'kappscores' ), 'kappscores', '<a href="https://mor10.com/courses" rel="designer">Morten Rand-Hendriksen</a>' ); ?></div>
+        </div><!-- .site-info -->
+    </div><!-- .site-footer__wrap -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
